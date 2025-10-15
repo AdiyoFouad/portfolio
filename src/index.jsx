520c287a70cde68662d984d5e13ab54024bffb16
AdiyoFouad <odjfouad38@gmail.com>
2025-10-15 18:12:23 +0200

    Component Footer

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import FabButton from './components/FabButton';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <FabButton/>
+      <Footer/>
+    </Router>
   </React.StrictMode>
 );
 
